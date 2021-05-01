a2022cb51d78c3bb80c5dc5267521ce23e54b765
Paulo Roberto <betinho5444@gmail.com>
2021-05-01 14:42:41 -0300

    feat: parsing some class comps to fun comps and understanding lifecycles

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,58 +1,67 @@
-import { Component } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
+export function Modal({ isOpen }: ModalProps) {
+
+  const [modalStatus, setModalStatus] = useState(isOpen)
+
+  const prevPropsRef = useRef();
+
+  useEffect(() => {
+    prevPropsRef.current = isOpen
+  })
+
+  const prevProps = prevPropsRef.current
+
+  useEffect(() => {
+
+    return () => {
+      if (prevProps !== isOpen) {
+        setModalStatus(isOpen)
+      }
     }
-  }
+  }, [])
+  // componentDidUpdate(prevProps) {
+  //   const { isOpen } = this.props;
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+  //   if (prevProps.isOpen !== isOpen) {
+  //     console.log(this.props)
+  //     this.setState({ modalStatus: isOpen })
+  //   }
+  // }
 
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
-      <ReactModal
-        shouldCloseOnOverlayClick={!false}
-        onRequestClose={setIsOpen}
-        isOpen={modalStatus}
-        ariaHideApp={false}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: '#F0F0F5',
-            color: '#000000',
-            borderRadius: '8px',
-            width: '736px',
-            border: 'none',
-          },
-          overlay: {
-            backgroundColor: '#121214e6',
-          },
-        }}
-      >
-        {children}
-      </ReactModal>
-    );
-  }
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
 };
 
 export default Modal;
---
+++ src/models/FoodsModel.ts
@@ -0,0 +1,8 @@
+export interface FoodsModel {
+  id: 1;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+}
